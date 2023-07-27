33832d8fe4a577498d381460784a3ac6d7220935
Rabiul Hasan <hassanmr866@gmail.com>
2023-07-27 14:33:12 +0600

    add all section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+   
+    extend: {
+      backgroundImaged : {
+        'background-pic': "url('images/banner1.png')",
+      }
+    },
+  },
+  plugins: [],
+}
